f6858c08b3ec8ffd6ee24389cb37fa1caf858cb3
MaxKompa <max.kompa645@gmail.com>
2026-05-07 15:21:46 +0200

    Added script, load to mysql database, added track dataset

---
+++ scripts/load_to_mysql.py
@@ -0,0 +1,21 @@
+﻿import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    "mysql+mysqlconnector://root:maxk62242@localhost/spotify_analytics"
+)
+
+df = pd.read_csv(
+    "../data/processed/clean_spotify_tracks.csv"
+)
+
+df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+df.to_sql(
+    "tracks",
+    con=engine,
+    if_exists="replace",
+    index=False
+)
+
+print("Данные загружены в MySQL!")
